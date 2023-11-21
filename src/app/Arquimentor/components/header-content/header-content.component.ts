25afda5e0f2203682bf2ae6aff40025169884d8f
leonardo <u202124304@upc.edu.pe>
2023-11-21 05:24:58 -0500

    feat(appointment): connection the appointment with mentor and student.

--- src/app/Arquimentor/components/header-content/header-content.component.ts
+++ src/app/Arquimentor/components/header-content/header-content.component.ts
@@ -1,5 +1,7 @@
 import {ChangeDetectorRef, Component} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
+import {StudentProfileService} from "../../services/student-profile.service";
+import {StudentProfile} from "../../model/student-profile";
 @Component({
   selector: 'app-header-content',
   templateUrl: './header-content.component.html',
@@ -7,32 +9,66 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class HeaderContentComponent {
   id: number=0;
+  studentProfile: StudentProfile | undefined
   constructor(private router:Router,
-              private route: ActivatedRoute
+              private route: ActivatedRoute,
+              private studentProfileService: StudentProfileService
   ) {
-
     this.route.queryParams.subscribe(params => {
       this.id = params['id'];
     });
+    console.log(this.id);
+
+
   }
   shouldRun = true;
 
   navigateHome() {
+    this.studentProfileService.getStudentProfileId(Number(this.id)).subscribe( (data: any) => {
+      console.log(data);
+      if (data != null) {
+        this.studentProfile = data;
+      }
+    });
     this.router.navigate(['home'],{ queryParams: { id: this.id } });
   }
 
   navigateSubscription() {
+    this.studentProfileService.getStudentProfileId(Number(this.id)).subscribe( (data: any) => {
+      console.log(data);
+      if (data != null) {
+        this.studentProfile = data;
+      }
+    });
     this.router.navigate(['subscription'],{ queryParams: { id: this.id } });
   }
 
   navigateMentorProfile() {
+    this.studentProfileService.getStudentProfileId(Number(this.id)).subscribe( (data: any) => {
+      console.log(data);
+      if (data != null) {
+        this.studentProfile = data;
+      }
+    });
     this.router.navigate([`mentor-profile/`+this.id],{ queryParams: { id: this.id } });
   }
 
   navigateStudentProfile() {
+    this.studentProfileService.getStudentProfileId(Number(this.id)).subscribe( (data: any) => {
+      console.log(data);
+      if (data != null) {
+        this.studentProfile = data;
+      }
+    });
     this.router.navigate(['student-profile/'+this.id],{ queryParams: { id: this.id } });
   }
   navigateSetting() {
+    this.studentProfileService.getStudentProfileId(Number(this.id)).subscribe( (data: any) => {
+      console.log(data);
+      if (data != null) {
+        this.studentProfile = data;
+      }
+    });
     this.router.navigate(['settings'],{ queryParams: { id: this.id } });
   }
 
