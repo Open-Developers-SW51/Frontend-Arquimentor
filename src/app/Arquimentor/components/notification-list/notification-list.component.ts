28653e8ae20d30c838c1d2c7b2139025eae46556
DiegoR434 <u202123548@upc.edu.pe>
2023-11-21 04:51:10 -0500

    feat(components): added notification-list components.

---
+++ src/app/Arquimentor/components/notification-list/notification-list.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {Notification} from "../../model/notification";
+import {NotificationService} from "../../services/notification.service";
+
+@Component({
+  selector: 'app-notification-list',
+  templateUrl: './notification-list.component.html',
+  styleUrls: ['./notification-list.component.css']
+})
+export class NotificationListComponent implements OnInit{
+  notifications: Notification[]=[];
+  constructor(private notificationService: NotificationService) { }
+
+  ngOnInit() {
+
+  }
+  showNotifications() {
+    this.loadNotifications();
+  }
+  loadNotifications(){
+    this.notifications = this.notificationService.getNotifications();
+  }
+}
